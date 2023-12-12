53b71379f11a03522b9fd26ba7fbf0dae057ed89
Bartosz Deptuła <barti.deptula@gmail.com>
2023-12-12 13:33:33 +0100

    version elevator added directly to setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,46 @@
 import setuptools
-from version_elevator import version
+import urllib.request
+import json
+
+from pkg_resources import parse_version
+
+MANUAL_VERSION = None
+
+
+def list_versions(package_name):
+    url = f"https://pypi.org/pypi/{package_name}/json"
+
+    with urllib.request.urlopen(url) as res:
+        data = json.loads(res.read().decode('utf-8'))
+
+    versions = data['releases']
+
+    return sorted(versions, key=parse_version, reverse=True)
+
+
+def version(new_version: bool = False) -> str:
+    """
+    Returns version of the new package, by elevating the version of the old package. Or returns the
+    manual version.
+    """
+    versions = list_versions('baca2-package-manager')
+    v = versions[0]
+    if len(versions) == 0:
+        v = '0.0.1'
+
+    if not new_version:
+        return v
+
+    if MANUAL_VERSION is not None:
+        mv = MANUAL_VERSION
+        if mv not in versions:
+            v = mv
+
+    new_v = v.split('.')
+    new_v[-1] = str(int(new_v[-1]) + 1)
+    new_v = '.'.join(new_v)
+    return new_v
+
 
 setuptools.setup(
     name='baca2-package-manager',
