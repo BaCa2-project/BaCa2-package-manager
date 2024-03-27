41586e68c5161e3d2504202c18c024265d34e386
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-03-27 14:38:12 +0100

    validators.py overhaul

--- baca2PackageManager/validators.py
+++ baca2PackageManager/validators.py
@@ -1,4 +1,7 @@
+import re
 from abc import ABC, abstractmethod
+from enum import Enum, auto
+from pathlib import Path
 from typing import Any, Iterable, Callable
 
 
@@ -24,10 +27,22 @@ class ValidatorNot(SettingsValidator):
 
 
 class ValidatorSeries(SettingsValidator):
-    operator_t = Callable[[Iterable[bool]], bool]
 
-    def __init__(self, operator: operator_t, operands: Iterable[SettingsValidator]):
-        self.operator = operator
+    class Operator(Enum):
+        AND = auto()
+        OR = auto()
+        XOR = auto()
+
+    def __init__(self, operator: Operator, operands: Iterable[SettingsValidator]):
+        match operator:
+            case self.Operator.OR:
+                self.operator = any
+            case self.Operator.AND:
+                self.operator = all
+            case self.Operator.XOR:
+                self.operator = lambda x: any(x) and not all(x)
+            case _:
+                raise NotImplemented
         self.operands = operands
 
     def validate(self, value: Any) -> bool:
@@ -37,20 +52,92 @@ class ValidatorSeries(SettingsValidator):
 class ValidatorFunc(SettingsValidator):
     func_t = Callable[[Any], bool]
 
-    def __init__(self, func: func_t):
+    def __init__(self, func: func_t, assert_type: type | tuple[type, ...] = None):
+        self.assert_type = assert_type
         self.func = func
 
+    @classmethod
+    def decorate(cls, assert_type: type | tuple[type, ...] = None):
+        return lambda func: cls(func, assert_type)
+
     def validate(self, value: Any) -> bool:
+        if self.assert_type is not None and not isinstance(value, self.assert_type):
+            return False
         return self.func(value)
 
 
 # ========================================================================
 
-def type_validator(types: type | tuple[type]) -> ValidatorFunc:
-    return ValidatorFunc(lambda value: isinstance(value, types))
+
+class IsNone(SettingsValidator):
+
+    def validate(self, value: Any) -> bool:
+        return value is None
 
 
-def range_validator(start: Any, stop: Any, include_start: bool = True, include_stop: bool = False) -> ValidatorFunc:
-    return ValidatorFunc(lambda value: start < value < stop
-                         or (include_start and value == start)
-                         or (include_stop and value == stop))
+class IsNotEmpty(SettingsValidator):
+
+    def validate(self, value: Any) -> bool:
+        return bool(value)
+
+
+class IsIn(SettingsValidator):
+
+    def __init__(self, valid_values: Iterable):
+        self.valid_values = valid_values
+
+    def validate(self, value: Any) -> bool:
+        return value in self.valid_values
+
+
+class IsType(SettingsValidator):
+
+    def __init__(self, type_: type | tuple[type, ...]):
+        self.type_ = type_
+
+    def validate(self, value: Any) -> bool:
+        return isinstance(value, self.type_)
+
+
+class IsExactly(SettingsValidator):
+
+    def __init__(self, value: Any):
+        self.value = value
+
+    def validate(self, value: Any) -> bool:
+        return value == self.value
+
+
+class IsRegexMatch(IsType):
+
+    def __init__(self, regex: str):
+        super().__init__(str)
+        self.regex = re.compile(regex)
+
+    def validate(self, value: Any) -> bool:
+        return super().validate(value) and bool(self.regex.match(value))
+
+
+class IsAlphanumeric(IsRegexMatch):
+
+    def __init__(self):
+        super().__init__(r'^[a-zA-Z0-9]+$')
+
+
+class IsPath(SettingsValidator):
+
+    def validate(self, value: Any) -> bool:
+        if isinstance(value, str):
+            value = Path(value)
+        return isinstance(value, Path) and value.exists()
+
+
+class IsRestrictedList(SettingsValidator):
+
+    def __init__(self, validator: SettingsValidator):
+        self.validator = validator
+
+    def validate(self, value: Any) -> bool:
+        if not isinstance(value, (list, tuple)):
+            return False
+        return all(map(self.validator.validate, value))
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,4 +1,5 @@
 import unittest
+from pathlib import Path
 
 import baca2PackageManager.validators as validators
 
@@ -9,6 +10,16 @@ class ValidatorsTest(unittest.TestCase):
         v1 = validators.ValidatorFunc(lambda x: 1 <= x < 3)
         self.assertTrue(v1(2))
         self.assertFalse(v1(10))
+        v2 = validators.ValidatorFunc(lambda x: x > 5, assert_type=int)
+        self.assertTrue(v2.validate(6))
+        self.assertFalse(v2.validate(6.))
+
+        @validators.ValidatorFunc.decorate(int)
+        def v3(x):
+            return x > 5
+
+        self.assertTrue(v3.validate(6))
+        self.assertFalse(v3.validate(6.))
 
     def test_validator_not(self):
         v1 = validators.ValidatorFunc(lambda x: 1 <= x < 3)
@@ -21,11 +32,58 @@ class ValidatorsTest(unittest.TestCase):
             validators.ValidatorFunc(lambda x: isinstance(x, float)),
             validators.ValidatorFunc(lambda x: x == 1)
         ]
-        v = validators.ValidatorSeries(any, v_list)
+        v = validators.ValidatorSeries(validators.ValidatorSeries.Operator.OR, v_list)
         self.assertTrue(v(1))
-        v = validators.ValidatorSeries(all, v_list)
-        self.assertFalse((v(1)))
-        self.assertTrue((v(1.)))
+        v = validators.ValidatorSeries(validators.ValidatorSeries.Operator.AND, v_list)
+        self.assertFalse(v(1))
+        self.assertTrue(v(1.))
+
+    def test_isin_validator(self):
+        v = validators.IsIn([1, 2, 3, 4])
+        self.assertTrue(v.validate(1))
+        self.assertFalse(v.validate(5))
+
+    def test_isnone(self):
+        v = validators.IsNone()
+        self.assertTrue(v.validate(None))
+        self.assertFalse(v.validate('None'))
+
+    def test_is_not_empty(self):
+        v = validators.IsNotEmpty()
+        self.assertTrue(v.validate('a'))
+        self.assertFalse(v.validate(''))
+
+    def test_istype(self):
+        v = validators.IsType((int, float))
+        self.assertTrue(v.validate(1))
+        self.assertTrue(v.validate(1.2))
+        self.assertFalse(v.validate('1.1'))
+
+    def test_is_exactly(self):
+        v = validators.IsExactly('42')
+        self.assertTrue(v.validate('42'))
+        self.assertFalse(v.validate(42))
+
+    def test_is_regex_match(self):
+        v = validators.IsRegexMatch(r'[A-Za-z.]+@[A-Za-z]{2,}\.(com|pl|org)')
+        self.assertTrue(v.validate('grzegorz.brzeczyszczykiewicz@gmail.com'))
+        self.assertFalse(v.validate('wrong_email@gmial.com'))
+
+    def test_is_alphanumeric(self):
+        v = validators.IsAlphanumeric()
+        self.assertTrue(v.validate('1234frfrnocap'))
+        self.assertFalse(v.validate('not. alpha. numeric.'))
+
+    def test_is_path(self):
+        v = validators.IsPath()
+        self.assertTrue(v.validate('/usr/bin'))  # Do not use Windows (bad practice)
+        self.assertFalse(v.validate('/does/not/exist'))
+        self.assertTrue(v.validate(Path('/usr/bin')))
+
+    def test_is_restricted_list(self):
+        v = validators.IsRestrictedList(validators.IsType(str))
+        self.assertTrue(v.validate(['a', 'b', 'c']))
+        self.assertFalse(v.validate(['a', 'b', 2]))
 
 
 if __name__ == '__main__':
